12be81187c0c08ea3a0256fb455f51497239c68b
Karel Vuong <vuongkarel@gmail.com>
2015-03-16 17:34:22 -0700

    Include d3.js and plottable.js

--- index.js
+++ index.js
@@ -2,5 +2,18 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-plottable'
+  name: 'ember-cli-plottable',
+
+  included: function(app) {
+    this._super.included(app);
+
+    var options = app.options.emberHighCharts || {includePlottableJS: true};
+
+    if (options.includePlottableJS) {
+      app.import('vendor/d3.min.js');
+      app.import('vendor/plottable.min.js');
+      app.import('vendor/plottable.css');
+    }
+
+  }
 };
